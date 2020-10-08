665fe1e13daca609d7abac53e33cc0e8f0415773
pawar4 <pawar4@purdue.edu>
2020-10-08 02:50:18 -0400

    Initial Structure class

--- Dungeon.h
+++ Dungeon.h
@@ -1,16 +1,19 @@
 #ifndef DUNGEON_H_
 #define DUNGEON_H_
 #include <iostream>
+#include "Room.h"
+#include "Creature.h"
+#include "Item.h"
 
 class Dungeon {
 
 public:
     Dungeon();
-    Dungeon& getDungeon(std::string name, int width, int gameHeight);
-    void addRoom(Room& room);
-    void addCreature(Creature& creature);
-    void addPassage(std::string name);
-    void addItem(Item& item);
+    virtual Dungeon& getDungeon(std::string name, int width, int gameHeight);
+    virtual void addRoom(Room& room);
+    virtual void addCreature(Creature& creature);
+    virtual void addPassage(std::string name);
+    virtual void addItem(Item& item);
 
 private:
     std::string name;
