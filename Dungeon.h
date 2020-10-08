6f986db530ca09c294bdd4a2ca7d7fff7772eed5
pawar4 <pawar4@purdue.edu>
2020-10-08 00:59:47 -0400

    Initial Dungeon class update

---
+++ Dungeon.h
@@ -0,0 +1,19 @@
+#ifndef DUNGEON_H_
+#define DUNGEON_H_
+#include <iostream>
+
+class Dungeon {
+
+public:
+    Dungeon();
+    Dungeon& getDungeon(std::string name, int width, int gameHeight);
+    void addRoom(Room& room);
+    void addCreature(Creature& creature);
+    void addPassage(std::string name);
+    void addItem(Item& item);
+
+private:
+
+};
+
+#endif
