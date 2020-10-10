5fb12a5ad810e56cf1b05429d91731174549f99a
BluSoD <54406500+BluSoD@users.noreply.github.com>
2020-10-10 15:39:39 -0400

    Actions Class first commit

---
+++ Action.cpp
@@ -0,0 +1,39 @@
+#include "Action.h"
+Action::Action(int v, int c) : v(v), c(c) {
+	std::cout << "Action Constructor" << std::endl;
+}
+Action::Action() {
+	std::cout << "Action Constructor" << std::endl;
+}
+void Action::setMessage(std::string msg) {
+	msg = msg;
+	std::cout << "Action::setMessage" << std::endl;
+}
+
+void Action::setIntValue(int v) {
+	v = v;
+	std::cout << "Action::setIntValue" << std::endl;
+}
+
+void Action::setCharValue(char c) {
+	c = c;
+	std::cout << "Action::setCharValue" << std::endl;
+}
+
+CreatureAction::CreatureAction(int owner) {
+	std::cout << "CreatureAction Constructor" << std::endl;
+}
+
+ItemAction::ItemAction(int owner) {
+	std::cout << "ItemAction Constructor" << std::endl;
+}
+
+BlessCurseOwner::BlessCurseOwner(int Owner)
+{
+	std::cout << "BlessCurseOwner Constructor" << std::endl;
+}
+
+Hallucinate::Hallucinate(int Owner)
+{
+	std::cout << "Hallucinate Constructor" << endl
+}
